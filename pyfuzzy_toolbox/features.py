5851dd1eb91a2a8974ee3d684c44a792439807c6
matheuscas <matheus.mcas@gmail.com>
2015-01-28 23:21:43 -0300

    added trigrams count method, but unteted due to lack of docs with trigrams

--- pyfuzzy_toolbox/features.py
+++ pyfuzzy_toolbox/features.py
@@ -162,3 +162,25 @@ def positive_to_negative_ratio_count_unigrams_and_bigrams_scores(bow_sentences, 
     positive_unigrams_and_bigrams_count = count_of_unigrams_and_bigrams_scores(bow_sentences, unigram=unigram, bigram_word_1=bigram_word_1, bigram_word_2=bigram_word_2)
     negative_unigrams_and_bigrams_count = count_of_unigrams_and_bigrams_scores(bow_sentences, unigram=unigram, bigram_word_1=bigram_word_1, bigram_word_2=bigram_word_2, positive=False)
     return positive_unigrams_and_bigrams_count - negative_unigrams_and_bigrams_count
+
+
+# UNTESTED
+def count_of_trigrams_scores(bow_sentences, bigram_word_1=ADVS, bigram_word_2=ADVS, bigram_word_3=ADJS, positive=True):
+
+    pos_tags_codes_word_1 = set_pos_tags_codes(bigram_word_1)
+    pos_tags_codes_word_2 = set_pos_tags_codes(bigram_word_2)
+    pos_tags_codes_word_3 = set_pos_tags_codes(bigram_word_3)
+
+    polarity_eval_stm = set_ngram_polarity_statement(positive=positive)
+
+    _count = 0
+    for bs in bow_sentences:
+        for ngram in bs:
+            if pre.is_trigram(ngram) and \
+                    (ngram.word_1.pos_tag in pos_tags_codes_word_1) and \
+                    (ngram.word_2.pos_tag in pos_tags_codes_word_2) and \
+                    (ngram.word_3.pos_tag in pos_tags_codes_word_3) and \
+                    eval(polarity_eval_stm):
+                    _count += 1
+
+    return _count
