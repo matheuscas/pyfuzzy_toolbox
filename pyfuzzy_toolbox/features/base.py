214d8d8b6c95e8e93d126af4e4b808b15c9d6faf
matheuscas <matheus.mcas@gmail.com>
2015-02-02 02:56:59 -0300

    changed the ref to the modules due to similarity with native functions

--- pyfuzzy_toolbox/features/base.py
+++ pyfuzzy_toolbox/features/base.py
@@ -1,7 +1,7 @@
 from . import ALL, ALL_NON_GENERAL_BIGRAMS
-import count
-import sum
-import max
+import count as count_feats
+import sum as sum_feats
+import max as max_feats
 
 REAL_POSITIVE_POLARITY = 1
 REAL_NEGATIVE_POLARITY = 0
@@ -28,40 +28,40 @@ def start(bow_sentences,
 
     features_list = []
     if count_features:
-        features_list = features_list + count.all(bow_sentences,
-                                                  unigrams_only=unigrams,
-                                                  unigrams_only_ratio=unigrams_ratio,
-                                                  unigram_type=unigram_type,
-                                                  non_general_unigrams_and_bigrams=non_general_unigrams_and_bigrams,
-                                                  non_general_unigrams_and_bigrams_ratio=non_general_unigrams_and_bigrams_ratio,
-                                                  non_general_bigram_type=non_general_bigram_type,
-                                                  ngrams_count=ngrams_count,
-                                                  general_unigrams_and_bigrams=general_unigrams_and_bigrams,
-                                                  general_unigrams_and_bigrams_ratio=general_unigrams_and_bigrams_ratio,
-                                                  unigrams_and_bigrams_trigram=unigrams_and_bigrams_trigram,
-                                                  unigrams_and_bigrams_trigram_ratio=unigrams_and_bigrams_trigram_ratio)
+        features_list = features_list + count_feats.all(bow_sentences,
+                                                        unigrams_only=unigrams,
+                                                        unigrams_only_ratio=unigrams_ratio,
+                                                        unigram_type=unigram_type,
+                                                        non_general_unigrams_and_bigrams=non_general_unigrams_and_bigrams,
+                                                        non_general_unigrams_and_bigrams_ratio=non_general_unigrams_and_bigrams_ratio,
+                                                        non_general_bigram_type=non_general_bigram_type,
+                                                        ngrams_count=ngrams_count,
+                                                        general_unigrams_and_bigrams=general_unigrams_and_bigrams,
+                                                        general_unigrams_and_bigrams_ratio=general_unigrams_and_bigrams_ratio,
+                                                        unigrams_and_bigrams_trigram=unigrams_and_bigrams_trigram,
+                                                        unigrams_and_bigrams_trigram_ratio=unigrams_and_bigrams_trigram_ratio)
 
     if sum_features:
-        features_list = features_list + sum.all(bow_sentences,
-                                                unigrams=unigrams,
-                                                unigrams_ratio=unigrams_ratio,
-                                                unigram_type=ALL,
-                                                non_general_unigrams_and_bigrams=non_general_unigrams_and_bigrams,
-                                                non_general_unigrams_and_bigrams_ratio=non_general_unigrams_and_bigrams_ratio,
-                                                non_general_bigram_type=non_general_bigram_type,
-                                                general_unigrams_and_bigrams=general_unigrams_and_bigrams,
-                                                general_unigrams_and_bigrams_ratio=general_unigrams_and_bigrams_ratio,
-                                                unigrams_and_bigrams_trigram=unigrams_and_bigrams_trigram,
-                                                unigrams_and_bigrams_trigram_ratio=unigrams_and_bigrams_trigram_ratio)
+        features_list = features_list + sum_feats.all(bow_sentences,
+                                                      unigrams=unigrams,
+                                                      unigrams_ratio=unigrams_ratio,
+                                                      unigram_type=unigram_type,
+                                                      non_general_unigrams_and_bigrams=non_general_unigrams_and_bigrams,
+                                                      non_general_unigrams_and_bigrams_ratio=non_general_unigrams_and_bigrams_ratio,
+                                                      non_general_bigram_type=non_general_bigram_type,
+                                                      general_unigrams_and_bigrams=general_unigrams_and_bigrams,
+                                                      general_unigrams_and_bigrams_ratio=general_unigrams_and_bigrams_ratio,
+                                                      unigrams_and_bigrams_trigram=unigrams_and_bigrams_trigram,
+                                                      unigrams_and_bigrams_trigram_ratio=unigrams_and_bigrams_trigram_ratio)
 
     if max_features:
-        features_list = features_list + max.all(bow_sentences,
-                                                unigrams=unigrams,
-                                                unigram_type=ALL,
-                                                non_general_unigrams_and_bigrams=non_general_unigrams_and_bigrams,
-                                                non_general_bigram_type=non_general_bigram_type,
-                                                general_unigrams_and_bigrams=general_unigrams_and_bigrams,
-                                                unigrams_and_bigrams_trigram=unigrams_and_bigrams_trigram)
+        features_list = features_list + max_feats.all(bow_sentences,
+                                                      unigrams=unigrams,
+                                                      unigram_type=ALL,
+                                                      non_general_unigrams_and_bigrams=non_general_unigrams_and_bigrams,
+                                                      non_general_bigram_type=non_general_bigram_type,
+                                                      general_unigrams_and_bigrams=general_unigrams_and_bigrams,
+                                                      unigrams_and_bigrams_trigram=unigrams_and_bigrams_trigram)
 
     attributes = []
     data = []
@@ -72,7 +72,7 @@ def start(bow_sentences,
 
     polarity_type = 'REAL'
     if type(bow_sentences_ground_polarity) == str:
-        polarity_type = ['positive','negative']
+        polarity_type = ['positive', 'negative']
 
     attributes.append(('polarity', polarity_type))
     data.append(bow_sentences_ground_polarity)
