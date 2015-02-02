93c55e635445c2c34eec9a4b5efcb59211ab354e
matheuscas <matheus.mcas@gmail.com>
2015-02-01 21:32:27 -0300

    added modules to package all"

--- pyfuzzy_toolbox/__init__.py
+++ pyfuzzy_toolbox/__init__.py
@@ -3,4 +3,5 @@
 __author__ = 'Matheus Cardoso'
 __email__ = 'matheus.mcas@gmail.com'
 __version__ = '0.1.0'
-__all__ = ["preprocessing"]
+__all__ = ["preprocessing", "intensifiers", "transformation",
+           "features", "util", "mf", "operators", "fis", "wm", "model"]
