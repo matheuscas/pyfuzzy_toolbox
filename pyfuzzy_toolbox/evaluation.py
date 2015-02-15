05022bbdb0cad553d08dd77fd47a75521210c0e2
matheuscas <matheus.mcas@gmail.com>
2015-02-15 04:59:51 -0300

    prevented division by zero

--- pyfuzzy_toolbox/evaluation.py
+++ pyfuzzy_toolbox/evaluation.py
@@ -1,14 +1,15 @@
 def precision(TP, FP):
-	return TP / (TP + FP)
+	print (TP + FP)
+	return (TP / (TP + FP)) if (TP + FP) > 0 else 0
 
 
 def recall(TP, FN):
-	return TP / (TP + FN)
+	return (TP / (TP + FN)) if (TP + FN) > 0 else 0
 
 
 def f1(TP, FP, TN, FN):
-	return (2 * (((TP / (TP + FP)) * (TP / (TP + FN))) / ((TP / (TP + FP)) + (TP / (TP + FN)))))
+	return ((2 * (((TP / (TP + FP)) * (TP / (TP + FN))) / ((TP / (TP + FP)) + (TP / (TP + FN)))))) if (TP + FP) > 0 and (TP + FN) > 0 else 0
 
 
 def accuracy(TP, FP, TN, FN):
-	return (TP + TN) / (TP + TN + FP + FN)
+	return ((TP + TN) / (TP + TN + FP + FN)) if (TP + TN + FP + FN) > 0 else 0
